16d048045acfdde05f86f526e5d0a0a700f36dd8
freshovich <freshovich@yahoo.com>
2022-03-17 01:07:09 +0300

    first lab - done

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -11,11 +11,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener{
 
     Button mainButton;
     TextView mainTextView;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ListView mainListView;
     ArrayAdapter mArrayAdapter;
     ArrayList mNameList = new ArrayList();
+    Button ok_btn, cnc_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,28 +36,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mainButton = findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
         mainEditText = (EditText) findViewById(R.id.main_edittext);
+
         mainListView = findViewById(R.id.main_listview);
         mArrayAdapter = new ArrayAdapter(this,
                 android.R.layout.simple_list_item_1, mNameList);
         mainListView.setAdapter(mArrayAdapter);
-        mNameList.add(mainEditText.getText().toString());
-        mArrayAdapter.notifyDataSetChanged();
-        mainListView.setOnClickListener(this);
+        mainListView.setOnItemClickListener(this);
+
+        ok_btn = findViewById(R.id.ok_btn);
+        cnc_btn = findViewById(R.id.cnc_btn);
+        ok_btn.setOnClickListener(oclBtn);
+        cnc_btn.setOnClickListener(oclBtn);
     }
     @Override
     public void onClick(View v){
-        mainTextView.setText("Button Pressed!");
-        mainTextView.setText(mainEditText.getText().toString()+"is learning Android development!");
-
+//        mainTextView.setText("Button Pressed!");
+//        mainTextView.setText(mainEditText.getText().toString()+"is learning Android development!");
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
     }
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-    {
-        Log.d("omg android", position + ": " + mNameList.get(position));
+    public void onItemClick(AdapterView<?>parent, View view, int position, long
+                            id) {
+        Log.d("omg android", position + ":" + mNameList.get(position));
         mainTextView.setText(mNameList.get(position).toString()
-                + "is learning Android development!");
-
+                + " is learning Android development!");
     }
+    View.OnClickListener oclBtn = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            // по id определ кнопку, вызывавшую этот обработчик
+            switch (v.getId()){
+                case R.id.ok_btn:
+                    // кнопка ОК
+                    mainTextView.setText("Нажата кнопка ОК");
+                    Toast.makeText(MainActivity.this, "Нажата кнопка ОК", Toast.LENGTH_SHORT).show();;
+                    break;
+                case R.id.cnc_btn:
+                    // кнопка cancel
+                    mainTextView.setText("Нажата кнопка cancel");
+                    break;
+            }
+
+        }
+    };
 
 
 }
